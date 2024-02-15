61a503398749fab9a9293d798d6e5a13807e3b63
Zahin Zawad <zahin.zawad.19@gmail.com>
2024-02-16 00:23:13 +0600

    adding yes function

---
+++ index.js
@@ -0,0 +1,17 @@
+var button = document.getElementById('Yes').addEventListener('click', function(){
+    console.log(123);
+});
+const button_no = document.getElementById('No');
+
+
+// const card_1 = document.querySelector('.container');
+// const card_2 = document.querySelector('.container-2');
+
+// button_yes.addEventListener('click' , function(){
+
+//     card_1.classList.add("hide");
+//     card_2.classList.remove("hide");
+//     console.log(123);
+    
+// } )
+
